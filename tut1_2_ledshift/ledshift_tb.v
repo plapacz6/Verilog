ca4d07c8d1d8da49eaa0669304f0853c5c545ccf
pawel <plapacz6@gmail.com>
2022-11-12 05:54:39 +0100

    adding simulation instructions

--- tut1_2_ledshift/ledshift_tb.v
+++ tut1_2_ledshift/ledshift_tb.v
@@ -1,8 +1,12 @@
+`timescale 1ns/10ps
+//`include "ledshift.v"
+
 module ledshift_tb();
 
 reg sys_clk, sys_rst;
 reg start, stop;
 wire [7:0] leds_tb;
+
 
 initial //only in simulation, not generate phisical circuit
   begin
@@ -17,13 +21,19 @@ initial //only in simulation, not generate phisical circuit
     start = #200 1;
     start = #200 0;
 
+    $monitor ($time, " | %d | %b", sys_clk, leds_tb);
+
     stop = #6000 1;
 
     //$monitor ($time, " | %d", sys_clk);
     $monitor ($time, " | %d | %b", sys_clk, leds_tb);
+    $dumpfile("led_shift.vcd");
+    $dumpvars(0, ledshift_tb);
+
+    $finish;
   end
 
-  always #50 sys_clk = -sys_clk;  //nanosecunds  50 == 10MHz
+always #50 sys_clk = -sys_clk;  //nanosecunds  50 == 10MHz
     ledshift UUT(
     .clk(sys_clk),
     .rst(sys_rst),
@@ -31,4 +41,5 @@ initial //only in simulation, not generate phisical circuit
     .i_stop(stop),
     .o_led(leds_tb)
     );
+
 endmodule //ledshift_tb
